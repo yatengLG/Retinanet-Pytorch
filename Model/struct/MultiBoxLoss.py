0ef3c20b23d8fbf19ed250831ee6fdd224f5ccd2
yatengLG <767624851@qq.com>
2019-09-24 11:34:06 +0800

    损失函数部分修改

--- Model/struct/MultiBoxLoss.py
+++ Model/struct/MultiBoxLoss.py
@@ -2,11 +2,14 @@
 # @Author  : LG
 import torch.nn as nn
 from .Focal_Loss import focal_loss
+import torch
+import torch.nn.functional as F
+import math
 
 __all__ = ['multiboxloss']
 
 class multiboxloss(nn.Module):
-    def __init__(self, cfg=None, alpha=None, gamma=None, num_classes=None):
+    def __init__(self, cfg=None, alpha=None, gamma=None, num_classes=None, neg_pos_ratio=None):
         """
         retainnet损失函数,分为类别损失(focal loss)
         框体回归损失(smooth_l1_loss)
@@ -17,12 +20,15 @@ class multiboxloss(nn.Module):
             self.alpha = cfg.MULTIBOXLOSS.ALPHA
             self.gamma = cfg.MULTIBOXLOSS.GAMMA
             self.num_classes = cfg.DATA.DATASET.NUM_CLASSES
+            self.neg_pos_ratio = cfg.TRAIN.NEG_POS_RATIO
         if alpha:
             self.alpha = alpha
         if gamma:
             self.gamma = gamma
         if num_classes:
             self.num_classes = num_classes
+        if neg_pos_ratio:
+            self.neg_pos_ratio = neg_pos_ratio
 
         self.loc_loss_fn = nn.SmoothL1Loss(reduction='sum')
         self.cls_loss_fn = focal_loss(alpha=self.alpha, gamma=self.gamma, num_classes=self.num_classes, reduction='sum')    # 类别损失为focal loss
@@ -39,7 +45,13 @@ class multiboxloss(nn.Module):
         """
         num_classes = confidence.size(2)
 
-        classification_loss = self.cls_loss_fn(confidence, labels)
+        with torch.no_grad():
+            # derived from cross_entropy=sum(log(p))
+            loss = -F.log_softmax(confidence, dim=2)[:, :, 0]
+            mask = hard_negative_mining(loss, labels, self.neg_pos_ratio)
+
+
+        classification_loss = self.cls_loss_fn(confidence[mask, :], labels[mask])
 
         # 回归损失,smooth_l1
         pos_mask = labels > 0
@@ -48,3 +60,22 @@ class multiboxloss(nn.Module):
         smooth_l1_loss = self.loc_loss_fn(predicted_locations, gt_locations)
         num_pos = gt_locations.size(0)
         return smooth_l1_loss / num_pos, classification_loss / num_pos
+
+def hard_negative_mining(loss, labels, neg_pos_ratio=3):
+    """
+    用于训练过程中正负例比例的限制.默认在训练时,负例数量是正例数量的三倍
+    Args:
+        loss (N, num_priors): the loss for each example.
+        labels (N, num_priors): the labels.
+        neg_pos_ratio:  正负例比例: 负例数量/正例数量
+    """
+    pos_mask = labels > 0
+    num_pos = pos_mask.long().sum(dim=1, keepdim=True)
+    num_neg = num_pos * neg_pos_ratio
+
+    loss[pos_mask] = -math.inf  # 无穷
+    # 两次sort 找出元素在排序中的位置
+    _, indexes = loss.sort(dim=1, descending=True)  # descending 降序 ,返回 value,index
+    _, orders = indexes.sort(dim=1)
+    neg_mask = orders < num_neg  # loss 降序排, 背景为-无穷, 选择排前num_neg的 负无穷,也就是 背景
+    return pos_mask | neg_mask  # 目标 或 背景
