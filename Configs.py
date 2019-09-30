8b275d8f37840c2c00b1de057e040671c796b328
yatengLG <767624851@qq.com>
2019-09-30 09:35:28 +0800

    现默认使用单gpu

--- Configs.py
+++ Configs.py
@@ -12,7 +12,7 @@ _C.FILE = CN()
 
 _C.FILE.PRETRAIN_WEIGHT_ROOT = project_root+'/Weights/pretrained'   # 会使用到的预训练模型
 _C.FILE.MODEL_SAVE_ROOT = project_root+'/Weights/trained'           # 训练模型的保存
-# _C.FILE.VGG16_WEIGHT = 'vgg16_reducedfc.pth'                        # vgg预训练模型
+# _C.FILE.VGG16_WEIGHT = 'vgg16_reducedfc.pth'                      # vgg预训练模型
 
 _C.DEVICE = CN()
 
